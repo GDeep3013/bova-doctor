50cc9587c4509e4fca01cb43e28b90a339d61f67
GaganDeep <gdeep3013@gmail.com>
2024-11-21 14:04:27 +0530

    sale

--- src/app/earnings/component/earnings.js
+++ src/app/earnings/component/earnings.js
@@ -2,15 +2,10 @@
 import AppLayout from 'components/Applayout';
 import DoctorGraph from './doctarGraph';
 import Sales from './Sales';
-import Loader from 'components/loader';
-import { useState } from 'react';
-
 export default function Earnings() {
-
-    const[fetchLoader,setFetchLoader]=useState(false)
     return (
         <AppLayout>
-                <>
+       <>
             <h1 className="text-2xl pt-4 md:pt-1 mb-1">Earnings</h1>
             <div className='grid grid-cols-12 gap-5 mt-6'>
                 <div className='col-span-12 xl:col-span-4'>
@@ -25,7 +20,7 @@ export default function Earnings() {
                 </div>
             </div>
             </>  
-             {/* } */}
+        
         </AppLayout>
     )
 }
