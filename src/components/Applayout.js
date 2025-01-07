f4ffbc5d045b004dfc16a3035356ef05a40d04a0
GaganDeep <gdeep3013@gmail.com>
2025-01-07 15:20:05 +0530

    order functionlity

--- src/components/Applayout.js
+++ src/components/Applayout.js
@@ -10,7 +10,7 @@ export default function AppLayout({ children }) {
             <Navbar isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
             <div className="dashboard-outer flex">
                 <Sidebar isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar } />
-                <div className='dashboard-right relative w-full transition ease-in-out delay-150'>
+                <div className='dashboard-right overflow-hidden relative w-full transition ease-in-out delay-150'>
                     <div className='dashboard-inner min-h pt-[20px] p-[20px] xl:p-[40px] md:pt-[30px]'>
                         {children}
                     </div>
