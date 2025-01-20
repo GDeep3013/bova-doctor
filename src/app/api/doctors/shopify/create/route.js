2914cbdf99a213594dd8880816016bfe1d7ddaaf
GaganDeep <gdeep3013@gmail.com>
2025-01-20 17:59:40 +0530

    shipping mangement

--- src/app/api/doctors/shopify/create/route.js
+++ src/app/api/doctors/shopify/create/route.js
@@ -3,15 +3,13 @@ import connectDB from '../../../../../db/db';
 import Doctor from '../../../../../models/Doctor';
 import { createProfile, subscribeProfiles, deleteProfile } from '../../../../klaviyo/klaviyo';
 
-connectDB(); // Ensure database connection is established
-
+connectDB();
 const APP_HEADERS = {
     'Access-Control-Allow-Origin': '*',  // replace with your actual origin if needed
     'Access-Control-Allow-Methods': 'GET, DELETE, PATCH, POST, PUT',
     'Access-Control-Allow-Credentials': 'true',
     'Content-Type': 'application/json',
-};
-// import { sendEmail } from '@/utils/email';
+  }
 
 export async function POST(req) {
     try {
@@ -34,13 +32,13 @@ export async function POST(req) {
         if (!zipCode || zipCode.trim() === '') errors.zipCode = 'Zip code is required';
 
         if (Object.keys(errors).length > 0) {
-            return new Response(JSON.stringify({ error: errors }), { status: 400, headers: APP_HEADERS });
+            return new Response(JSON.stringify({ error: errors }), { status: 400, headers: APP_HEADERS, });
         }
 
         const query = {
             $or: [
                 { email },
-                ...(phone ? [{ phone }] : []) 
+                ...(phone ? [{ phone }] : [])
             ]
         };
         const existingDoctor = await Doctor.findOne(query);
@@ -52,10 +50,10 @@ export async function POST(req) {
                 errors.phone = 'Phone number already exists';
             }
         }
-        
+
         // If errors exist, return a keyed response
         if (Object.keys(errors).length > 0) {
-            return new Response(JSON.stringify({ error: errors }), { status: 400, headers: APP_HEADERS });
+            return new Response(JSON.stringify({ error: errors }), { status: 400, headers: APP_HEADERS, });
         }
         await Doctor.create({
             firstName,
@@ -69,62 +67,57 @@ export async function POST(req) {
             zipCode
         });
 
-        
-        // try {
-        //     const user = {
-        //         email: "sahil.610weblab@gmail.com",
-        //         firstName:"sahil",
-        //         lastName:"Dharmani"
-        //     };
-        //     const customProperties = {
-        //         firstName:firstName,
-        //         lastName:lastName,
-        //         doctor_email:email,
-        //         phone:phone,              
-        //         address:address,
-        //         state:state,
-        //         city:city,
-        //         zipCode:zipCode              
-        //     };
-        //     console.log(user,customProperties)
+        try {
+            const user = { email: process.env.ADMIN_EMAIL, firstName: process.env.ADMIN_FIRST_NAME, lastName: process.env.ADMIN_LAST_NAME };
 
-        //     const listId = 'YxYgt4';
+            const customProperties = {
+                firstName: firstName,
+                lastName: lastName,
+                doctor_email: email,
+                phone: phone,
+                address: address,
+                state: state,
+                city: city,
+                zipCode: zipCode
+            };
 
-        //     setTimeout(async () => {
-        //         try {
-        //             await deleteProfile(user);
-        //         } catch (error) {
-        //             console.error('Error deleting profile:', error);
-        //         }
-        //     }, 60000);
-        //     const createProfilePromise = createProfile(user, customProperties);
-        //     const subscribeProfilePromise = subscribeProfiles(user, listId);
+            const listId = 'YxYgt4';
 
-        //     setTimeout(async () => {
-        //         try {
-        //             const deleteProfileResponse = await deleteProfile(user);
-        //         } catch (error) {
-        //             console.error('Error deleting profile:', error);
-        //         }
-        //     }, 60000);
+            setTimeout(async () => {
+                try {
+                    await deleteProfile(user);
+                } catch (error) {
+                    console.error('Error deleting profile:', error);
+                }
+            }, 60000);
+            const createProfilePromise = createProfile(user, customProperties);
+            const subscribeProfilePromise = subscribeProfiles(user, listId);
 
-        //     const [createResponse, subscribeResponse] = await Promise.all([
-        //         createProfilePromise,
-        //         subscribeProfilePromise,
-        //     ]);
-        // } catch (error) {
-        //     console.error('Error handling Klaviyo actions:', error);
-        // }
+            setTimeout(async () => {
+                try {
+                    const deleteProfileResponse = await deleteProfile(user);
+                } catch (error) {
+                    console.error('Error deleting profile:', error);
+                }
+            }, 60000);
+
+            const [createResponse, subscribeResponse] = await Promise.all([
+                createProfilePromise,
+                subscribeProfilePromise,
+            ]);
+        } catch (error) {
+            console.error('Error handling Klaviyo actions:', error);
+        }
 
 
         return new Response(
             JSON.stringify({
                 message: 'Doctor created  successfully',
             }),
-            { status: 201, headers: APP_HEADERS }
+            { status: 201, headers: APP_HEADERS,}
         );
     } catch (error) {
         console.error('Error in POST request:', error);
-        return new Response(JSON.stringify({ error: error.message }), { status: 500, headers: APP_HEADERS });
+        return new Response(JSON.stringify({ error: error.message }), { status: 500, headers: APP_HEADERS, });
     }
 }
