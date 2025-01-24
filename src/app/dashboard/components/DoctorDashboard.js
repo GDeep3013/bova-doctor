37e8790b8260952ef52824131185e482b178fef8
GaganDeep <gdeep3013@gmail.com>
2025-01-24 13:58:52 +0530

    update changes

--- src/app/dashboard/components/DoctorDashboard.js
+++ src/app/dashboard/components/DoctorDashboard.js
@@ -23,7 +23,7 @@ export default function AdminDashboard() {
             }
             const data = await response.json();
             if (data) {
-               setTotalEarning(data.totalEarnings);
+                setTotalEarning(data.totalEarnings);
                 setTotalPatients(data.totalPatients);
                 setTotalPlans(data.totalPlans);
                 setFetchLoader(false)
@@ -59,30 +59,30 @@ export default function AdminDashboard() {
     return (
         <AppLayout>
             <div className="flex flex-col">
-                {fetchLoader ?<Loader/>:
-                <>
-                <div className='doctor-graph max-w-5xl'>
-                    <div className="min-[1025px]:mt-0 flex max-[575px]:gap-y-4 min-[576px]:space-x-5 max-[575px]:flex-wrap">
-                        {cards.map((card, index) => (
-                            <div
-                                key={index}
-                                className="flex justify-between items-center bg-[#F9F9F9] rounded-lg p-3 lg:p-6 w-full shadow-sm"
-                            >
-                                <div>
-                                    <h3 className="text-md lg:text-xl xl:text-2xl  font-bold text-[#53595B]">{card.title}</h3>
-                                    <p className="text-xs lg:text-sm xl:text-base mt-1 text-gray-500">{card.subtitle}</p>
-                                </div>
-                                <div className="flex-shrink-0 bg-[#EBEDEB] w-[31px] h-[31px] md:w-[41px] md:h-[41px] rounded-[5px] shadow-sm relative card-icon mr-2 md:mr-0">
-                                    <div className="text-3xl text-[#53595B] absolute card-svg">
-                                        {card.icon}
+                {fetchLoader ? <Loader /> :
+                    <>
+                        <div className='doctor-graph max-w-5xl'>
+                            <div className="min-[1025px]:mt-0 flex max-[575px]:gap-y-4 min-[576px]:space-x-5 max-[575px]:flex-wrap">
+                                {cards.map((card, index) => (
+                                    <div
+                                        key={index}
+                                        className="flex justify-between items-center bg-[#F9F9F9] rounded-lg p-3 lg:p-6 w-full shadow-sm"
+                                    >
+                                        <div>
+                                            <h3 className="text-md lg:text-xl xl:text-2xl  font-bold text-[#53595B]">{card.title}</h3>
+                                            <p className="text-xs lg:text-sm xl:text-base mt-1 text-gray-500">{card.subtitle}</p>
+                                        </div>
+                                        <div className="flex-shrink-0 bg-[#EBEDEB] w-[31px] h-[31px] md:w-[41px] md:h-[41px] rounded-[5px] shadow-sm relative card-icon mr-2 md:mr-0">
+                                            <div className="text-3xl text-[#53595B] absolute card-svg">
+                                                {card.icon}
+                                            </div>
+                                        </div>
                                     </div>
-                                </div>
+                                ))}
                             </div>
-                        ))}
-                            </div>
-                </div>
-                <AddPatient/>
-                </>}
+                        </div>
+                        <AddPatient />
+                    </>}
             </div>
         </AppLayout>
     )
